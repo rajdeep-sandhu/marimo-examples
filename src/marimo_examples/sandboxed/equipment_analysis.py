8fa06e53f53853868ac836259a2b8e65aac13b10
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-06-27 14:40:16 +0000

    add title

--- src/marimo_examples/sandboxed/equipment_analysis.py
+++ src/marimo_examples/sandboxed/equipment_analysis.py
@@ -9,11 +9,19 @@ __generated_with = "0.23.11"
 app = marimo.App(width="full", app_title="Equipment Analysis")
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Equipment Transaction Analysis
+    """)
+    return
+
+
 @app.cell
 def _():
     import marimo as mo
 
-    return
+    return (mo,)
 
 
 if __name__ == "__main__":
