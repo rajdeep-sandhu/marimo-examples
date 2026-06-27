49041260754f50097a06d7d36bdaabeeb9ba493a
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-06-27 20:38:51 +0000

    add file_to_df()

--- src/marimo_examples/sandboxed/equipment_analysis.py
+++ src/marimo_examples/sandboxed/equipment_analysis.py
@@ -31,8 +31,17 @@ def _():
 
 
 @app.cell
+def _():
+    from io import BytesIO
+
+    import duckdb
+    import polars as pl
+
+    return BytesIO, pl
+
+
+@app.cell
 def _(mo):
-    # Upload equipment data file
     file_input = mo.ui.file(
         label="Upload Equipment Transaction Data (`.xlsx`)",
         filetypes=[".xlsx"],
@@ -40,6 +49,44 @@ def _(mo):
     )
 
     file_input
+    return (file_input,)
+
+
+@app.cell
+def _(BytesIO, pl):
+    def file_to_df(file_input) -> dict:
+        """
+        Returns the filenme and a polars dataframe from a .xlsx file uploaded via mo.ui.file.
+
+        params:
+        file_input: The file input element's value.
+        """
+
+        # Get first result for single file upload
+        result = file_input.value[0]
+        filename: str = result.name
+        content: bytes = result.contents
+        df: pl.DataFrame = pl.read_excel(source=BytesIO(content))
+
+        return {"filename": filename, "data": df}
+
+    return (file_to_df,)
+
+
+@app.cell
+def _(file_input, file_to_df, pl):
+    upload_result: dict | None = None
+
+    if file_input.value:
+        upload_result = file_to_df(file_input)
+        ux_trans_filename: str = upload_result["filename"]
+        ux_trans: pl.DataFrame = upload_result["data"]
+    return (ux_trans,)
+
+
+@app.cell
+def _(ux_trans: "pl.DataFrame"):
+    ux_trans
     return
 
 
